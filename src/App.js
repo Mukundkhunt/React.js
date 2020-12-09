792e40c1ee1a7e95fc5a49ccb43ebc2281fdd960
Mukund Khunt <64716112+Mukundkhunt@users.noreply.github.com>
2020-12-09 23:55:22 +0530

    init

--- src/App.js
+++ src/App.js
@@ -1,19 +1,24 @@
 import React, { Component } from 'react'
-import { Route, Switch } from 'react-router-dom'
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
+import '../node_modules/bootstrap/dist/js/bootstrap.bundle'
+import { Redirect, Route, Switch } from 'react-router-dom'
 import About from './About'
 import Contact from './Contact'
 import Home from './Home'
 import Service from './Service'
+import Navbar from './Navbar'
 
 export default class App extends Component {
   render() {
     return (
       <>
+        <Navbar />
         <Switch>
-          <Route path="/" component={Home} />
-          <Route path="/about" component={About} />
-          <Route path="/service" component={Service} />
-          <Route path="/contact" component={Contact} />
+          <Route exact path="/" component={Home} />
+          <Route exact path="/about" component={About} />
+          <Route exact path="/service" component={Service} />
+          <Route exact path="/contact" component={Contact} />
+          <Redirect to="/" />
         </Switch>
       </>
     )
